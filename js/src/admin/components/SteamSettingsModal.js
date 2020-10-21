d198441ca8bdca3d4cae2d69c79828e204866f05
NomisCZ <NomisCZ@users.noreply.github.com>
2020-10-21 21:46:08 +0200

    Update extension to new Flarum release (#18)
    - Use FoF components package for admin section
    - Add missing Guzzle Client dependency #12 to composer.json

--- js/src/admin/components/SteamSettingsModal.js
+++ js/src/admin/components/SteamSettingsModal.js
@@ -1,7 +1,16 @@
+import { settings } from '@fof-components';
 import SettingsModal from 'flarum/components/SettingsModal';
-import BooleanItem from "./settings/BooleanItem";
+
+const {
+    items: { BooleanItem, StringItem },
+} = settings;
 
 export default class SteamSettingsModal extends SettingsModal {
+    oninit(vnode) {
+        super.oninit(vnode);
+        this.setting = this.setting.bind(this);
+    }
+
     className() {
         return 'AuthSteamSettingsModal Modal--small';
     }
@@ -13,12 +22,17 @@ export default class SteamSettingsModal extends SettingsModal {
     form() {
         return [
             <div className="Form-group">
-                <label>{app.translator.trans('nomiscz-auth-steam.admin.settings.api_key_label')}</label>
-                <input className="FormControl" bidi={this.setting('flarum-ext-auth-steam.api_key')}/>
+                <StringItem
+                    name={'flarum-ext-auth-steam.api_key'}
+                    setting={this.setting}
+                    {...{ ['required']: true }}
+                >
+                    {app.translator.trans(`nomiscz-auth-steam.admin.settings.api_key_label`)}
+                </StringItem>
             </div>,
             <div className="Form-group">
-                <BooleanItem key={'flarum-ext-auth-steam.use_steam_powered_domain'}>
-                    {app.translator.trans('nomiscz-auth-steam.admin.settings.use_steam_powered_domain')}
+                <BooleanItem name={'flarum-ext-auth-steam.use_steam_powered_domain'} setting={this.setting}>
+                    <span>{app.translator.trans('nomiscz-auth-steam.admin.settings.use_steam_powered_domain')}</span>
                 </BooleanItem>
             </div>
         ];
