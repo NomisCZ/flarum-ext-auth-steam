69770211a0a6671a457e92816a9e3aa7353b2cb1
NomisCZ <NomisCZ@users.noreply.github.com>
2021-02-04 18:01:31 +0100

    Update extension to Flarum v0.1.0-beta.15 (#21)
    - Fix frontend admin settings 'page'
    - Rewrite Dispatcher to ApiSerializer
    - Update dependencies

--- src/Providers/SteamAuth.php
+++ src/Providers/SteamAuth.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of nomiscz/flarum-ext-auth-steam.
  *
- * Copyright (c) 2019 NomisCZ.
+ * Copyright (c) 2021 NomisCZ.
  *
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
