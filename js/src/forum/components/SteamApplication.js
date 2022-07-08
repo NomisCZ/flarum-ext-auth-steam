bf111a2da54edb2e52c428325c3291c93a3003bb
Ngo Quoc Dat <datlechin@gmail.com>
2022-07-08 23:40:41 +0700

    Update import & run format

--- js/src/forum/components/SteamApplication.js
+++ js/src/forum/components/SteamApplication.js
@@ -1,30 +1,27 @@
-export default class SteamApplication
-{
-    linkDone(returnCode) {
+export default class SteamApplication {
+  linkDone(returnCode) {
+    let alert;
 
-        let alert;
-
-        switch (returnCode) {
-
-            case 'already_linked':
-                app.modal.close();
-                alert = app.alerts.show({type: 'error'}, app.translator.trans('nomiscz-auth-steam.forum.alerts.already_linked'));
-                break;
-            case 'already_used':
-                alert = app.alerts.show({type: 'error'}, app.translator.trans('nomiscz-auth-steam.forum.alerts.already_used'));
-                break;
-            case 'done':
-                app.modal.close();
-                app.session.user.savePreferences();
-                alert = app.alerts.show({type: 'success'}, app.translator.trans('nomiscz-auth-steam.forum.alerts.link_success'));
-                break;
-            case 'error':
-                alert = app.alerts.show({type: 'error'}, app.translator.trans('nomiscz-auth-steam.forum.alerts.error'));
-                break;
-        }
-
-        setTimeout(() => {
-            app.alerts.dismiss(alert);
-        }, 5000);
+    switch (returnCode) {
+      case 'already_linked':
+        app.modal.close();
+        alert = app.alerts.show({ type: 'error' }, app.translator.trans('nomiscz-auth-steam.forum.alerts.already_linked'));
+        break;
+      case 'already_used':
+        alert = app.alerts.show({ type: 'error' }, app.translator.trans('nomiscz-auth-steam.forum.alerts.already_used'));
+        break;
+      case 'done':
+        app.modal.close();
+        app.session.user.savePreferences();
+        alert = app.alerts.show({ type: 'success' }, app.translator.trans('nomiscz-auth-steam.forum.alerts.link_success'));
+        break;
+      case 'error':
+        alert = app.alerts.show({ type: 'error' }, app.translator.trans('nomiscz-auth-steam.forum.alerts.error'));
+        break;
     }
+
+    setTimeout(() => {
+      app.alerts.dismiss(alert);
+    }, 5000);
+  }
 }
--- js/src/forum/components/SteamLinkModal.js
+++ js/src/forum/components/SteamLinkModal.js
@@ -2,42 +2,50 @@ import Modal from 'flarum/components/Modal';
 import Button from 'flarum/components/Button';
 
 export default class SteamLinkModal extends Modal {
-    className() {
-        return 'AuthSteamLinkModal Modal--small';
-    }
+  className() {
+    return 'AuthSteamLinkModal Modal--small';
+  }
 
-    title() {
-        return app.translator.trans('nomiscz-auth-steam.forum.modals.link.title');
-    }
+  title() {
+    return app.translator.trans('nomiscz-auth-steam.forum.modals.link.title');
+  }
 
-    content() {
-        return (
-            <div className="Modal-body">
-                <div className="Form Form--centered">
-                    <div className="Form-group">
-                        <Button className={'Button LogInButton--steam'} icon={'fab fa-steam-symbol'} loading={this.loading} disabled={this.loading}
-                                path={`/auth/${name}`} onclick={() => this.showSteamLogin()}>
-                            {app.translator.trans('nomiscz-auth-steam.forum.buttons.login')}
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  content() {
+    return (
+      <div className="Modal-body">
+        <div className="Form Form--centered">
+          <div className="Form-group">
+            <Button
+              className={'Button LogInButton--steam'}
+              icon={'fab fa-steam-symbol'}
+              loading={this.loading}
+              disabled={this.loading}
+              path={`/auth/${name}`}
+              onclick={() => this.showSteamLogin()}
+            >
+              {app.translator.trans('nomiscz-auth-steam.forum.buttons.login')}
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-    showSteamLogin() {
+  showSteamLogin() {
+    const width = 600;
+    const height = 400;
+    const $window = $(window);
 
-        const width = 600;
-        const height = 400;
-        const $window = $(window);
+    window.open(
+      app.forum.attribute('apiUrl') + '/auth/steam/link',
+      'steamLinkPopup',
+      `width=${width},` +
+        `height=${height},` +
+        `top=${$window.height() / 2 - height / 2},` +
+        `left=${$window.width() / 2 - width / 2},` +
+        'status=no,scrollbars=no,resizable=no'
+    );
 
-        window.open(app.forum.attribute('apiUrl') + '/auth/steam/link', 'steamLinkPopup',
-            `width=${width},` +
-            `height=${height},` +
-            `top=${$window.height() / 2 - height / 2},` +
-            `left=${$window.width() / 2 - width / 2},` +
-            'status=no,scrollbars=no,resizable=no');
-
-        this.loading = true;
-    }
+    this.loading = true;
+  }
 }
